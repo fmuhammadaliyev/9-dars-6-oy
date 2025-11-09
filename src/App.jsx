143574d42f316b0a2d33cbd490345dc64310be26
muhammadaliyev <muhammadaliyev>
2025-11-09 15:17:48 +0500

    mf

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure className="px-10 pt-10">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="flex w-52 flex-col gap-4">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>
+    </div>
+  );
+}
